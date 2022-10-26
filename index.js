ab4cf3c7ed511169e1b57a6475470a3c6bae2388
Echo <58097612+3kh0@users.noreply.github.com>
2022-10-26 16:24:58 +0000

    commit

--- index.js
+++ index.js
@@ -5,27 +5,20 @@ const kuler = require('kuler');
 const app = express();
 
 const proxyUrl = "3kh0.github.io"
+console.log(kuler('Proxying the domain: ' + proxyUrl, '#00ffdd'));
 app.get("/*", async (req, res) => {
   const proxied = await fetch("https://" + proxyUrl + req.url);
   const mime = proxied.headers.get("content-type");
-  //console.log(mime.split(";")[0].split("/")[0])
-  if (mime.split(";")[0].split("/")[0] == "image") {
-    const image = proxied.body;
-    res.set("Content-Type",mime.split(";")[0])
-    res.send(image);
-
-  } else {
-    //console.log("Mime: " + mime);
-    res.set("Content-Type",mime.split(";")[0])
-    const html = await proxied.text();
-    res.send(html)
-  }
+  res.set("Content-Type",mime.split(";")[0])
+  const body = proxied.body
+  res.status(200);
+  res.write(body); // ERR_INVALID_ARG_TYPE 15:7
+  res.end();
 });
 
-// Here we start the app no shit
+// Here we start the proxy
 app.listen(3000, () => {
   console.log(kuler('Server has been started! Listening on port 3000', '#00ff00'));
-  console.log(kuler('Proxying the domain: ' + proxyUrl, '#00ffdd'));
   console.log("Link to view: " + kuler(`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`, '#0000ff'));
 
 });
