13539594a4d3017988df28d6c427b89d249497bd
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-11-26 16:52:40 -0600

    Type module and removed express (#3)
    * Type module and removed express
    
    * This should have been gitignored

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require("express");
-const fetch = require("node-fetch"); // How we are actually proxying the assets
-const kuler = require("kuler"); // Console colors
-const app = express();
+import http from 'node:http';
+import fetch from 'node-fetch'; // How we are actually proxying the assets
+import kuler from 'kuler'; // Console colors
 
-const proxyUrl = "3kh0.github.io";
-console.log(kuler("Proxying the domain: " + proxyUrl, "#00ffdd"));
-app.get("/*", async (req, res) => {
-  const proxied = await fetch("https://" + proxyUrl + req.url); // Get the asset from the website
-  const mime = proxied.headers.get("content-type"); // Send the correct MIME type so the browser knows what it is
-  res.set("Content-Type", mime.split(";")[0]); // Setting the MIME type
-  const body = new Buffer.from(await proxied.arrayBuffer()); // Get the buffer of the asset
-  res.status(proxied.status); // Send the correct HTTP Response back
-  res.write(body); // Write the buffered asset to the response
-  res.end();
-});
-
-// Here we start the proxy
-app.listen(3000, () => {
-  console.log(`
+console.log(`
 ██████╗ ██╗   ██╗███████╗███████╗██╗   ██╗
 ██╔══██╗██║   ██║██╔════╝██╔════╝╚██╗ ██╔╝
 ██████╔╝██║   ██║█████╗  █████╗   ╚████╔╝ 
 ██╔══██╗██║   ██║██╔══╝  ██╔══╝    ╚██╔╝  
 ██████╔╝╚██████╔╝██║     ██║        ██║   
 ╚═════╝  ╚═════╝ ╚═╝     ╚═╝        ╚═╝                                          
-`)
-  console.log(kuler("Server has been started! Listening on port 3000", "#00ff00"));
+`);
+
+const server = http.createServer();
+const url = "https://3kh0.github.io";
+const PORT = process.env.PORT || 8080;
+
+server.on('request', async (req, res) => {
+    const asset = await fetch(url + req.url); // Get the asset from the website
+    const body = new Buffer.from(await asset.arrayBuffer()); // Get the buffer of the asset
+    res.writeHead(asset.status, { "Content-Type": asset.headers.get("content-type").split(";")[0] }); // Send the HTTP status code set the MIME type
+    res.end(body); // Write the asset to the response
+});
+
+server.on('listening', () => {
+  console.log(kuler(`Server has been started! Listening on port ${PORT}`, "#00ff00"));
   console.log("Link to view: " + kuler(`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`, "#0000ff"));
 });
+
+// Here we start the proxy
+server.listen({ port: PORT })
--- replit.nix
+++ replit.nix
@@ -1,8 +1,5 @@
 { pkgs }: {
 	deps = [
 		pkgs.nodejs-16_x
-        pkgs.nodePackages.typescript-language-server
-        pkgs.yarn
-        pkgs.replitPackages.jest
 	];
 }
