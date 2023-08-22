9088b63086c78900ce3e8ba7c205afbc8079c99b
retronbv <retronbv@gmail.com>
2023-08-22 13:03:52 -0400

    some simple changes (v1.1.0)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import http from 'node:http';
 import fetch from 'node-fetch'; // How we are actually proxying the assets
 import kuler from 'kuler'; // Console colors
+import contentType from 'content-type'; // Get the MIME type of the asset
+import { hostname } from 'node:os'; // Computer's hostname
 
 console.log(`
 ██████╗ ██╗   ██╗███████╗███████╗██╗   ██╗
@@ -17,14 +19,18 @@ const PORT = process.env.PORT || 8080;
 
 server.on('request', async (req, res) => {
     const asset = await fetch(url + req.url); // Get the asset from the website
-    const body = new Buffer.from(await asset.arrayBuffer()); // Get the buffer of the asset
-    res.writeHead(asset.status, { "Content-Type": asset.headers.get("content-type").split(";")[0] }); // Send the HTTP status code set the MIME type
-    res.end(body); // Write the asset to the response
+    res.writeHead(asset.status, { "Content-Type": contentType.parse(asset.headers.get("content-type")).type }); // Send the HTTP status code set the MIME type
+    res.end(Buffer.from(await asset.arrayBuffer())); // Write the asset to the response
 });
 
 server.on('listening', () => {
   console.log(kuler(`Server has been started! Listening on port ${PORT}`, "#00ff00"));
-  console.log("Link to view: " + kuler(`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`, "#0000ff"));
+  if (process.env.REPL_SLUG && process.env.REPL_OWNER) console.log("Link to view: " + kuler(`https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`, "#0000ff"));
+  else {
+    console.log(kuler(`Links to view: http://${hostname()}:${PORT}`, "#0000ff"));
+    console.log(kuler(`              http://127.0.0.1:${PORT}`, "#0000ff"));
+    console.log(kuler(`              http://0.0.0.0:${PORT}`, "#0000ff"));
+  }
 });
 
 // Here we start the proxy
